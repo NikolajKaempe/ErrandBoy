a3d43f557f0ad36f8e89978c4489fd505bf378b9
Emil Madsen <Ostebob1234@gmail.com>
2016-05-31 15:17:53 +0200

    removed comments

--- app/src/main/java/dk/kea/class2016february/emilmadsen/ErrandBoy/ErrandBoy/WorldRenderer.java
+++ app/src/main/java/dk/kea/class2016february/emilmadsen/ErrandBoy/ErrandBoy/WorldRenderer.java
@@ -21,25 +21,21 @@ public class WorldRenderer
 
     public void render()
     {
-        System.out.println("***************************** THERE ARE " + world.lane1.size() + " Cars in Lane 1 ***");
         for (Car c : world.lane1){
             game.drawBitmap(cars,(int)c.x,(int)c.y,
                     c.currentBitmap.srcX,c.currentBitmap.srcY,
                     c.currentBitmap.width,c.currentBitmap.height);
         }
-        System.out.println("***************************** THERE ARE " + world.lane2.size() + " Cars in Lane 2 ***");
         for (Car c : world.lane2){
             game.drawBitmap(cars, (int) c.x, (int) c.y,
                     c.currentBitmap.srcX, c.currentBitmap.srcY,
                     c.currentBitmap.width, c.currentBitmap.height);
         }
-        System.out.println("***************************** THERE ARE " + world.lane3.size() + " Cars in Lane 3 ***");
         for (Car c : world.lane3){
             game.drawBitmap(cars, (int) c.x, (int) c.y,
                     c.currentBitmap.srcX, c.currentBitmap.srcY,
                     c.currentBitmap.width, c.currentBitmap.height);
         }
-        System.out.println("***************************** THERE ARE " + world.lane4.size() + " Cars in Lane 4 ***");
         for (Car c : world.lane4){
             game.drawBitmap(cars, (int) c.x, (int) c.y,
                     c.currentBitmap.srcX, c.currentBitmap.srcY,
